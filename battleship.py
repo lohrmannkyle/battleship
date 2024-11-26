ecd1bec5056cef5bdaac218c61203694693a8072
kyle <lohrmann_kyle@yahoo.com>
2024-11-26 09:08:54 -0700

    wow lots lots

--- battleship.py
+++ battleship.py
@@ -19,7 +19,10 @@ class GridPos:
         return self._ship
     
     def add_ship(self, ship):
-        self._ship = ship
+        if self._ship == None:
+            self._ship = ship
+            return True
+        return False
     
     def __str__(self):
         return "[{},{}]".format(self._x, self._y) if self._ship is None else "[{}]".format(self._ship.get_type() * 3)
@@ -27,8 +30,15 @@ class GridPos:
 class Board:
     def __init__(self):
         self._board = []
+        self._ships = {}
+        self._duplicate = False
         self.gen_board()
-        self._ships = []
+
+    def duplicate(self):
+        return self._duplicate
+
+    def ships(self):
+        return self._ships
     
     def gen_board(self):
         for i in range(10):
@@ -44,72 +54,146 @@ class Board:
         string = ""
         for i in range (9, -1, -1):
             for j in range(10):
-                string += str(self._board[i][j])
+                string += str(self._board[j][i])
             string += "\n"
         return string
     
     def get_gridpos(self, x, y):
         return self._board[x][y]
     
-    def valid_guess(x, y):
+    def valid_guess(self, x, y):
         return (x < 10 and x > -1) and (y < 10 and y > -1)
+    
+    def game_over(self):
+        return len(self._ships) == 0
     
     def guess(self, x, y):
         """
         Checks to see if a ship exists on the guessed gridsquare. 
         """
+        if not self.valid_guess(x, y):
+            print("illegal guess")
+            return 
+
         gridpos = self.get_gridpos(x,y)
         ship = gridpos.ship()
-        if ship and not ship.guessed:
+        if ship and not gridpos.guessed():
             ship.hit(x,y)
             gridpos.mark_guessed()
-            return True
+            if ship.sunk():
+                self._ships.pop(ship.get_type())
+                print("{} sunk".format(ship))
+            
+            if self.game_over():
+                print("all ships sunk: game over") 
+                sys.exit(0)
+        else:
+            print("miss")
         return False
     
-    def add_ship(self, gridpos, ship):
-        self._ships[gridpos] = ship
+    def add_ship_to_grid(self, ship, i, j):
+        return self.get_gridpos(i,j).add_ship(ship)
+    
+    def add_ship_to_ships(self, ship):
+        if ship.get_type() in self.ships().keys():
+            self._duplicate = True
+
+        self._ships[ship.get_type()] = ship
+
+
 
 class Ship:
     def __init__(self, type):
         self._type = type
         self._size = SHIP_SIZES[type]
         self._occupies = []
-        self._remaining = None
+        self._remaining = self._size
     
     def hit(self, x, y):
-        self._remaining.remove((x,y))
+        self._remaining -= 1
+        print("hit")
 
     def get_type(self):
         return self._type
+    
+    def sunk(self):
+        return self._remaining == 0
 
-    def occupies(self, start_x, stop_x, start_y, stop_y, board_obj):
+    def place(self, start_x, stop_x, start_y, stop_y, board_obj, line):
+
+        start_x, stop_x = min(start_x, stop_x), max(start_x, stop_x)
+        start_y, stop_y = min(start_y, stop_y), max(start_y, stop_y)
+
         if stop_x == start_x:
             for i in range(start_y, stop_y + 1):
                 self._occupies.append([start_x,i])
-                board_obj.get_board()[start_x][i].add_ship(self)
+                board_obj.add_ship_to_grid(self, start_x, i)
 
-        if start_y == stop_y:
+        elif start_y == stop_y:
             for i in range(start_x, stop_x + 1):
-                self._occupies.append([i ,start_x])
-                board_obj.get_board()[i][start_x].add_ship(self)
+                self._occupies.append([i ,start_y])
+                overlap = not board_obj.add_ship_to_grid(self, i, start_y)
+                if overlap:
+                    print("ERROR: overlapping ship: " + line)
+                    sys.exit(0)  
+        else:
+            "ERROR: ship not horizontal or vertical: " + line
+            sys.exit(0)
 
-def parse_file(self, file, board):
-    for line in file:
+        board_obj.add_ship_to_ships(self)
+
+    def __str__(self):
+        return "{}".format(self.get_type())
+
+def gen_board(placement_file):
+    board = Board()
+    for line in placement_file:
         line = line.split()
         type = line[0]
-        start_x, stop_x = line[2] ,line[4]
-        start_y, stop_y = line[1], line[3]
+        start_x, stop_x = int(line[1]) ,int(line[3])
+        start_y, stop_y = int(line[2]), int(line[4])
 
-        ship = Ship(type)
+        if max(start_x, stop_x, start_y, stop_y) > 9 or \
+            min(start_x, stop_x, start_y, stop_y) < 0:
+            print("ERROR: ship out-of-bounds: " + line)
+            sys.exit(0)
+
+
+        ship = Ship(type)        
+        ship.place(start_x, stop_x, start_y, stop_y, board, line)
+
+        if len(ship._occupies) != SHIP_SIZES[ship.get_type()]:
+            print("ERROR: incorrect ship size: " + line)
+            sys.exit(0)
+
+    return board
+
+def process_guesses(guess_file, board):
+    for line in guess_file:
+        line = line.split()
+        x, y = int(line[0]), int(line[1])
+
+        board.guess(x, y)
+
 
 
 
 def main():
-    board = Board()
-    # file = open(input())
-    print(board)
-    ship = Ship("B")
-    ship.occupies(4, 8, 5, 5,board)
-    print(board)
+
+    # placement_file = open("test/example-placement.txt")
+    # guess_file = open("test/example-guess.txt")
+    # board = gen_board(placement_file)
+    placement_file = open(input())
+    guess_file = open(input())
+    board = gen_board(placement_file)
+
+    if len(board.ships().keys()) != 5 or board.duplicate():
+        print("ERROR: fleet composition incorrect")
+        sys.exit(0)
+
+    # print(board)
+    # print(board._ships)
+
+    process_guesses(guess_file, board)
 
 main()
